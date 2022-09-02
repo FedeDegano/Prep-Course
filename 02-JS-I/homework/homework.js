bed076b972067fea23b00821d0e99f07de6dd37a
Facundo Degano <fededegano@gmail.com>
2022-09-02 11:16:01 -0300

    tarea html

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,8 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString ='hola'
+const nuevaString ='hola
+'
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -28,7 +29,6 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
 }
 
 function suma(x, y) {
